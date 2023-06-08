c8c6ffd0f2c8da92408846b0496c48fb54b5adca
Oryatory <larin.ia28@gmail.com>
2023-06-09 00:27:59 +0400

    some refactoring

---
+++ src/data/slides.ts
@@ -0,0 +1,64 @@
+export const slides = [
+  {
+    image:
+      "https://centrezotov.ru/wp-content/uploads/2023/04/logos-vystavka-1-884x650.jpg",
+    title: "Логос: голос конструктивизма",
+    date: "25.05 - 06.08",
+  },
+  {
+    image:
+      "https://centrezotov.ru/wp-content/uploads/2023/05/rannyaya-multiplikacziya-konki-884x650.jpg",
+
+    title: "Киноклавиши. Ранняя мультипликация",
+    date: "06.06",
+  },
+  {
+    image:
+      "https://centrezotov.ru/wp-content/uploads/2023/05/1920h650_lekcziya_-7-iyunya-884x650.png",
+
+    title: "Дмитрий Никишов. Коллаж. Фотомонтаж",
+    date: "07.06",
+  },
+  {
+    image:
+      "https://centrezotov.ru/wp-content/uploads/2022/12/mk-14.01-art-meditacziya-884x650.jpg",
+
+    title: "Антонина Бородина. Арт-медитация",
+    date: "10.06",
+  },
+  {
+    image:
+      "https://centrezotov.ru/wp-content/uploads/2023/05/beat-film-festival-884x650.jpg",
+
+    title: "Beat Film Festival",
+    date: "10.06 - 18.06",
+  },
+  {
+    image:
+      "https://centrezotov.ru/wp-content/uploads/2023/05/les_olympiades_dsc07529_r©shannabesson-884x650.jpg",
+
+    title: "Специальный показ. Париж, 13-й округ / Les Olympiades",
+    date: "11.06",
+  },
+  {
+    image:
+      "https://centrezotov.ru/wp-content/uploads/2023/05/funkczionalno-o_-hamovniki-884x650.png",
+
+    title: "Функционально о: Хамовники",
+    date: "11.06",
+  },
+  {
+    image:
+      "https://centrezotov.ru/wp-content/uploads/2023/04/44209-884x650.jpg",
+
+    title: "Кино крохам: выбор «Мела»",
+    date: "13.05 - 24.06",
+  },
+  {
+    image:
+      "https://centrezotov.ru/wp-content/uploads/2023/05/devushka-s-korobkoj-884x650.jpg",
+
+    title: "Фильм-концерт. Круглый Бенд х Девушка с коробкой",
+    date: "04.08",
+  },
+];
